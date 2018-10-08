ccdfd8d3e6e285352236ec1db5cac48b4de32b5e
Krisztián Gajdár <gajdikuka00@gmail.com>
2018-10-08 16:58:05 +0200

    bejelentkezes, regisztracio, elfelejtett helszo
    design

---
+++ alkalmazás/design teszt/design teszt/Views/LoginView.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace design_teszt.Views
+{
+    /// <summary>
+    /// Interaction logic for LoginView.xaml
+    /// </summary>
+    public partial class LoginView : UserControl
+    {
+        public LoginView()
+        {
+            InitializeComponent();
+        }
+
+        public static event EventHandler Bejelentkezes;
+        public static event EventHandler Regisztracio;
+        public static event EventHandler Elfelejtett;
+
+        public void login(object sender, RoutedEventArgs e)
+        {
+            if (Bejelentkezes != null)
+            {
+                Bejelentkezes(this, e);
+            }
+        }
+
+        private void register(object sender, RoutedEventArgs e)
+        {
+            if (Regisztracio != null)
+            {
+                Regisztracio(this, e);
+            }
+        }
+
+        private void forgotten(object sender, RoutedEventArgs e)
+        {
+            if (Elfelejtett != null)
+            {
+                Elfelejtett(this, e);
+            }
+        }
+    }
+}
