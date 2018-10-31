3baa054339ff649dd00fc9b076dd3956f76110d9
tesa307 <tesa307@gmail.com>
2018-10-31 16:40:48 +0100

    Validációs alap, de ne ezt csináld

---
+++ alkalmazás2/202MoneiBasicValidation/design teszt/Rules/PasswordMinimumCharacterRule.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+using System.Windows.Controls;
+
+namespace design_teszt.Rules
+{
+    public class PasswordMinimumCharacterRule : ValidationRule
+    {
+        public int PasswordMinimumCharacter { get; set; }
+
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            string charString = value as string;
+            if (charString.Length < PasswordMinimumCharacter || charString.Length == null)
+            {
+                return new ValidationResult(false, $"A jelszó nem lehet üres és {PasswordMinimumCharacter} hosszúnak kell lennie!");
+            }
+            return new ValidationResult(true, null);
+        }
+    }
+}
