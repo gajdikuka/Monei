b76f48fb505c4b14db230da6870021aa97a1a989
horvath95d <43348597+horvath95d@users.noreply.github.com>
2018-11-15 19:42:46 +0100

    Bejelentkezés
    LoginView.xaml.cs és LoginViewModel.cs   szerkesztése

--- Project/monei project/ViewModels/LoginViewModel.cs
+++ Project/monei project/ViewModels/LoginViewModel.cs
@@ -14,22 +14,23 @@ namespace monei_project.ViewModels
 {   
     class LoginViewModel : ViewModelBase
     {
-        private string _username;
+        private static string _username;
 
-        public string Username
+        public static string Username
         {
             get { return _username; }
             set { _username = value; }
         }
 
-        private string _password;
+        private static string _password;
 
-        public string Password
+        public static string Password
         {
             get { return _password; }
             set { _password = value; }
         }
 
+        //szerintem az egész DoLoginra nincs szükség
         private ICommand _doLogin;
         public ICommand DoLogin
         {
@@ -61,5 +62,13 @@ namespace monei_project.ViewModels
             return LoginView.HasValidationError;
         }
 
+        public static bool LoginChecking()
+        {
+            //if (getUsername(Username) && Password == Password(Username)) static metódusok kellenek :/
+            if (Username == "asd" && Password == "asd")
+                return true;
+            else
+                return false;
+        }
     }
 }
