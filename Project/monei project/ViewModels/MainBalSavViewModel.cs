4f087d3d8cff1c39120f724927a6c0623bc8ea59
tesa307 <tesa307@gmail.com>
2018-11-16 19:52:57 +0100

    DoBackgroundColor

--- Project/monei project/ViewModels/MainBalSavViewModel.cs
+++ Project/monei project/ViewModels/MainBalSavViewModel.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using monei_project.Commands;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace monei_project.ViewModels
 {
@@ -17,12 +19,46 @@ namespace monei_project.ViewModels
         }
 
         private string _username = Globals.LoggedInUser;
-        private string Username
+        public string Username
         {
             get { return _username; }
             set { _username = value; }
         }
 
+        private string _backGroundColor;
+
+        public string BackGroundColor
+            {
+                get { return _backGroundColor; }
+                set { _backGroundColor = value; OnPropertyChanged("BackGroundColor"); }
+            }
+
+
+        ICommand _doBackGroundColor;
+
+        public ICommand DoBackGroundColor
+        {
+            get
+            {
+                if (_doBackGroundColor == null)
+                {
+                    _doBackGroundColor = new RelayCommand(CanDoBackGroundColor, DoBackGroundColorExecute);
+                }
+                return _doBackGroundColor;
+            }
+        }
+
+        private bool CanDoBackGroundColor(object obj)
+        {
+            return true;
+        }
+
+        private void DoBackGroundColorExecute(object obj)
+        {
+            BackGroundColor = "#000000";
+        }
+
+       
     }
 
 }
--- Project/monei project/ViewModels/MainFelsoViewModel.cs
+++ Project/monei project/ViewModels/MainFelsoViewModel.cs
@@ -1,20 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
+using System.Linq.Expressions;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace monei_project.ViewModels
 {
     class MainFelsoViewModel : ViewModelBase
     {
 
-        private string _balance;
+        private string _balance = "11111$";
         public string Balance
         {
             get { return _balance; }
-            set { SetAndRaisePropertyChanged(ref _balance, "1111$"); }
+            set { SetAndRaisePropertyChanged(ref _balance, value); }
         }
 
         public void SetBalance()
