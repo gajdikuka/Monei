be18887510f58d1c5446399d54ecd5b98f0065e5
tesa307 <tesa307@gmail.com>
2018-12-05 17:54:58 +0100

    UserHandler+

--- Project/monei project/Handler/UserHandler.cs
+++ Project/monei project/Handler/UserHandler.cs
@@ -11,6 +11,11 @@ namespace monei_project.Operations
     class UserHandler
     {
         public string userName{ get; set; }
+        private bool getUser = false;
+        private bool getAccounts = false;
+        private User user;
+        private Account account;
+        private List<Transaction> transaction;
 
         public UserHandler(string userName)
         {
@@ -19,8 +24,35 @@ namespace monei_project.Operations
 
         private void GetUser()
         {
-            UserManager um = new UserManager(); 
-        } 
+            UserManager um = new UserManager();
+            User user = um.getUserByName(userName);
+            getUser = true;
+        }
+
+        private void GetAccounts()
+        {
+            if(!getUser)
+            {
+                throw new Exception("Először futtatnod kell a GetUser metódust!");
+            }
+            else
+            {
+                AccountManager am = new AccountManager();
+                account = am.getAccounts(user)[0];
+            }         
+        }
+
+        private void GetTransactions()
+        {
+            if(!getUser || !getAccounts)
+            {
+                throw new Exception("Először futtatnod kell a GetUser és GetAccounts metódusokat!");
+            }
+            else
+            {
+
+            }
+        }
 
 
     }
